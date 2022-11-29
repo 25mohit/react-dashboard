dcd993af6f9728d809cfc24f07084ccda32d5ee0
mohit agarwal <mohit724agarwal@gmail.com>
2022-11-29 11:05:03 +0530

    Dashboard Empty Screen Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './Style.css'
+import './Animation.css'
 import './App.css'
+import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Navigate, Outlet, Route, Routes, useNavigate } from 'react-router-dom'
 import StaticSidebar from './components/Common/StaticSidebar/StaticSidebar'
+import EmptyPage from './components/Common/EmptyPage/EmptyPage'
 import videoBG from './assets/Background/backgound_video.mp4'
 import MessageUI from './components/UI/MessageUI/MessageUI'
-import { useEffect, useState } from 'react'
 import ListedUI from './components/UI/ItemsUI/ListedUI'
 import DraftUI from './components/UI/ItemsUI/DraftUI'
 import ScheduledUI from './components/UI/ItemsUI/ScheduledUI'
@@ -20,9 +22,10 @@ import UsersSetting from './components/UI/DashboardUI/TabsUI/UsersSetting'
 function App() {
   const [toogleSidebar, setToogleSidebar] = useState(false)
 
+  const location = window.location.href.split('/')[3]
 
   return (
-    <Router>
+    <Router baseName={'/'}>
         <div className="top">
               <video src={videoBG} autoPlay muted loop/>
         </div>
@@ -31,25 +34,26 @@ function App() {
                   <StaticSidebar />
               </div> }
               <div className={toogleSidebar ? 'full-width' : 'body-main'}>
-                  <Routes>
-                        <Route path='/dashboard' element={<DashboardUI />} >
-                            <Route path='home' element={<HomeUI />}/>
-                            <Route path='pages' element={<PagesUI />}/>
-                            <Route path='quick-settings' element={<QuickSettings />}/>
-                            <Route path='user-settings' element={<UsersSetting />}/>
-                        </Route>
-                        <Route path='sales'>
-                            <Route path='orders' exact element={<OrdersUI />} />
-                        </Route>
-                        <Route path='items'>
-                            <Route path='listed-item' exact element={<ListedUI />} />
-                            <Route path='draft-item' exact element={<DraftUI />} />
-                            <Route path='scheduled-item' exact element={<ScheduledUI />} />
-                            <Route path='unsold-item' exact element={<UnsoldUI />} />
-                        </Route>
-                        <Route path='message' exact element={<MessageUI />} />
-                        <Route path='add-new-item' exact element={<AddNewItem /> } />
-                  </Routes>
+                    <Routes>
+                            <Route path='/' element={<EmptyPage />} />
+                            <Route path='dashboard' element={<DashboardUI />} >
+                                <Route path='home' element={<HomeUI />}/>
+                                <Route path='pages' element={<PagesUI />}/>
+                                <Route path='quick-settings' element={<QuickSettings />}/>
+                                <Route path='user-settings' element={<UsersSetting />}/>
+                            </Route>
+                            <Route path='sales'>
+                                <Route path='orders' exact element={<OrdersUI />} />
+                            </Route>
+                            <Route path='items'>
+                                <Route path='listed-item' exact element={<ListedUI />} />
+                                <Route path='draft-item' exact element={<DraftUI />} />
+                                <Route path='scheduled-item' exact element={<ScheduledUI />} />
+                                <Route path='unsold-item' exact element={<UnsoldUI />} />
+                            </Route>
+                            <Route path='message' exact element={<MessageUI />} />
+                            <Route path='add-new-item' exact element={<AddNewItem /> } />
+                    </Routes>
               </div>
         </div>
     </Router>
---
+++ src/components/Common/EmptyPage/EmptyPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EmptyPage = () => {
+  return (
+    <div className='empty-page'>
+      <h1>Welcome to your Premium Fully Customizable React Admin Dashboard</h1>
+      <p>Go to Dashboard</p>
+    </div>
+  )
+}
+
+export default EmptyPage
