39ffc9bc19c9a0f677e6af2effd16f06451b562d
mohit agarwal <mohit724agarwal@gmail.com>
2022-12-26 23:40:44 +0530

    Premium Card UI Completed for Dashboard

--- src/components/Common/Cards/PremiumCard.jsx
+++ src/components/Common/Cards/PremiumCard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BsCheck2 } from 'react-icons/bs'
+import { MdReportGmailerrorred } from 'react-icons/md'
 
-const PremiumCard = () => {
+const PremiumCard = ({ design }) => {
   return (
-    <div>PremiumCard</div>
+    <div className={`premium-card-UI ${design}-card`} >
+        <h3>Basic</h3>
+        <div className="underline" />
+        <div className="container">
+            <h1>$99</h1>
+            <p>Price Example</p>
+            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</span>
+            <div>
+                <ul>
+                  <li><BsCheck2 style={{color:'green'}}/>&nbsp;&nbsp;Tradition Offer</li>
+                  <li><BsCheck2 style={{color:'green'}}/>&nbsp;&nbsp;Premium Offer</li>
+                  <li><MdReportGmailerrorred style={{color:'rgb(255, 39, 39)'}}/>&nbsp;&nbsp;Exclusive Offer</li>
+                </ul>
+                <button>Get This Plan</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/UI/DashboardUI/TabsUI/CardsUI.jsx
+++ src/components/UI/DashboardUI/TabsUI/CardsUI.jsx
@@ -3,7 +3,19 @@ import PremiumCard from "../../../Common/Cards/PremiumCard"
 const CardsUI = () => {
   return (
     <div className="cards-main">
-        <PremiumCard />
+        <h1 className="row-heading">Premium Card</h1>
+        <div className="row">
+            <PremiumCard design='original'/>
+            <PremiumCard design='red'/>
+            <PremiumCard design='green'/>
+            <PremiumCard design='blue'/>
+            <PremiumCard design='orange'/>
+            <PremiumCard design='yellow'/>
+            <PremiumCard design='white'/>
+        </div>
+        <h1 className="row-heading">Classic Card</h1>
+        <div className="row">
+        </div>
     </div>
   )
 }
